3989b2b0bccd2e87b22b3831f1fd6790bb31a8ec
unknown <you@example.com>
2023-10-22 20:54:22 +0200

    configure env and claster

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = 3000;
-
-
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -12,20 +10,14 @@ app.use(express.json());
 app.use("/", require("./routes"));
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/library")
-    .then(() => console.log("MongoDB connected"))
-    .catch((error) => console.log(error.message));
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error.message));
 
-
-
-
-
-
-
-app.listen(PORT, (error) => {
-    if (error) {
-        console.log(error.message);
-    } else {
-        console.log(`Server running on http://localhost:${PORT}`);
-    }
+app.listen(process.env.PORT, (error) => {
+  if (error) {
+    console.log(error.message);
+  } else {
+    console.log("Server running");
+  }
 });
