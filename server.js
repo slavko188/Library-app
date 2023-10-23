07b16477c884f529a0b55cdd7e6c1a091fb28da4
unknown <you@example.com>
2023-10-23 18:41:16 +0200

    Portfolio

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", require("./routes"));
