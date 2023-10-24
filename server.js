16d48adfb3c1a8892a8fb30c051cb8ae5e14e733
unknown <you@example.com>
2023-10-24 14:19:03 +0200

    new env

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+//Use midlwere
+
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 app.use("/", require("./routes"));
 
+//connection with base
+
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error.message));
+
+// server
 
 app.listen(process.env.PORT, (error) => {
   if (error) {
